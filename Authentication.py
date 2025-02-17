7f17c4e73272d32384ea7d114ec20cdda9693241
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-02-17 11:29:53 +0200

    configurating firebase

--- Authentication.py
+++ Authentication.py
@@ -1,1 +1,18 @@
 import pyrebase
+
+firebaseConfig = {
+
+  "apiKey": "AIzaSyBI8cLQei5DiNAdNqI4tHKqDEmK2zRTNNY",
+
+  "authDomain": "skill-sync-cli.firebaseapp.com",
+
+  "projectId": "skill-sync-cli",
+
+  "storageBucket": "skill-sync-cli.firebasestorage.app",
+
+  "messagingSenderId": "280182390752",
+
+  "appId": "1:280182390752:web:637d7d746129658fb27585",
+
+  "measurementId": "G-HP8LXVM2ME"}
+
