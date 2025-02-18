021f97358c8f27ba1ae6ede7a04fb54bde7c70ba
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-02-18 09:29:38 +0200

    defining a forgot password funtion

--- Authentication.py
+++ Authentication.py
@@ -22,33 +22,47 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 
 def sign_up():
   auth = firebase.auth()
-  email = input("please enter your email: ")
+  name = input("please enter your name: ")
+  role = input("please enter your role(mentor/peer): ").lower()
 
+  if role not in ["mentor", "peer"]:
+        print("Invalid role. Please enter 'mentor' or 'peer'.")
+        return
+
+  db = firebase.database()
+  email = input("please enter your email: ")
   password = input("please enter a password: ")
 
+  #validating password
   if len(password) < 8:
+    print("password must be at least 8 characters long")
     return
-
   #check if password contains at least one uppercase letter
   if not re.search("[A-Z]", password):
+    print("password must contain  uppercase letter")
     return
-
   #check if password contains at least one lowercase letter
   if not re.search("[a-z]", password):
+    print("password must contain  lowercase letter")
     return
-
   #check if password contains at least one digit
   if not re.search("[0-9]", password):
+    print("password must contain  digit")
     return
-
   #check if password contains at least one special character
   if not re.search("[^A-Za-z0-9]", password):
+    print("password must contain  special character")
     return 
   
-  
-  
   try:
-    auth.create_user_with_email_and_password(email, password)
+    user = auth.create_user_with_email_and_password(email, password)
+    uid = user["localId"]
+    user_data = {"name": name,
+                "role": role,
+                "email": email}
+    
+    db.child("users").child(uid).push(user_data, user["idToken"])
+
     print("User created successfully")
   except Exception as e:
     print("Error: ", str(e)) 
@@ -60,22 +74,33 @@ def log_in():
     password = input("Please enter your password: ")
     
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
+        auth.sign_in_with_email_and_password(email, password)
         print("Login successful! Welcome,", email)
     except Exception as e:
         print("Error: Incorrect password or email.", str(e))
 
+def forgot_password():
+  auth = firebase.auth()
+  email = input("Please enter your email: ")
+  try:
+    auth.send_password_reset_email(f"email:{email}")
+    print("Password reset email sent to your email")
+  except Exception as e:
+      print("Error: ", str(e))
+
 
 if __name__ == "__main__":
     while True:
-        choice = input("\n1. Sign Up\n2. Log In\n3. Exit\nChoose an option: ")
+        choice = input("\n1. Sign Up\n2. Log In\n 3, forgot password?\n4. Exit\nChoose an option: ")
         if choice == "1":
-            sign_up()
+          sign_up()
         elif choice == "2":
-            log_in()
+          log_in()
         elif choice == "3":
-            print("Exiting program.")
-            break
+          forgot_password()
+        elif choice == "4":
+          print("Exiting program.")
+          break
         else:
             print("Invalid option, please try again.")
 
