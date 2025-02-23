8b2b5348382739b3555cdd9dacd283befe553b6a
memulimjhb024 <memulimjhb024@student.wethinkcode.co.za>
2025-02-23 16:42:14 +0200

    Add Credentials.json to .gitignore

--- calender_api.py
+++ calender_api.py
@@ -1,18 +1,20 @@
 import os
-import pickle
+import json
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-# If modifying these SCOPES, delete the file token.pickle.
+# If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/calendar/v3']
 
 def authenticate_google_calendar():
     creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    if os.path.exists('token.json'):
+        with open('token.json', 'r') as token:
+            creds_data = json.load(token)
+            creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
+    
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -20,8 +22,11 @@ def authenticate_google_calendar():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        
+        # Save the credentials as a JSON file
+        with open('token.json', 'w') as token:
+            json.dump(creds.to_json(), token)
+    
     return build('calendar', 'v3', credentials=creds)
 
 def create_event(service, summary, start_time, end_time, timezone, attendees=None):
