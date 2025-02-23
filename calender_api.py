1e7dc047ea7af9973f5ceabe9255cfc1858a157c
MenelikMayaba <mayabamulima147@gmail.com>
2025-02-23 16:16:16 +0200

    Create calender_api.py
    installed google calender API modules.
    created a function that intergrates the google calender

---
+++ calender_api.py
@@ -0,0 +1,30 @@
+import os
+import json
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If modifying these SCOPES, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/calendar/v3']
+
+def authenticate_google_calendar():
+    creds = None
+    if os.path.exists('token.json'):
+        with open('token.json', 'r') as token:
+            creds_data = json.load(token)
+            creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        # Save the credentials as a JSON file
+        with open('token.json', 'w') as token:
+            json.dump(creds.to_json(), token)
+    
+    return build('calendar', 'v3', credentials=creds)
