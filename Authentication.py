2394358958d959407a37f964eefa3213e64bc709
MenelikMayaba <mayabamulima147@gmail.com>
2025-02-23 16:07:22 +0200

    Update Authentication.py

--- Authentication.py
+++ Authentication.py
@@ -10,14 +10,15 @@ firebaseConfig = {
 
   "projectId": "skill-sync-cli",
 
+  "databaseURL": "https://skill-sync-cli-default-rtdb.firebaseio.com/",
+  
   "storageBucket": "skill-sync-cli.firebasestorage.app",
 
   "messagingSenderId": "280182390752",
 
   "appId": "1:280182390752:web:637d7d746129658fb27585",
 
-  "measurementId": "G-HP8LXVM2ME",
-  "databaseURL": ""}
+  "measurementId": "G-HP8LXVM2ME"}
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
@@ -26,24 +27,16 @@ current_user= 0
 
 @click.command()
 @click.option('--email', prompt='Email', help='Email to use for signup')
-@click.option('--passowrd', prompt= True, hide_input = True, confirmation_prompt = True, help='Password to use for signup')
+@click.option('--password', prompt= True, hide_input = True, confirmation_prompt = True, help='Password to use for signup')
 @click.option('--name', prompt='full name', help='Name to use for signup')
-@click.option('--role', type=click.Choice(['peer or mentor']), prompt ='role', help='Role to use for signup')
+@click.option('--role', type=click.Choice(['peer', 'mentor']), prompt ='role', help='Role to use for signup')
 @click.option("--expertise", prompt="Expertise (if mentor)", default="", help="Enter expertise (optional for peers)")
-def sign_up():
-  
-  name = input("please enter your name: ").capitalize()
-  role = input("please enter your role(mentor/peer): ").lower()
-  expertise = input("what do you expertise in: ").lower()
+def sign_up(name, email, password, role, expertise):
 
   if role not in ["mentor", "peer"]:
         print("Invalid role. Please enter 'mentor' or 'peer'.")
         return
-
   
-  email = input("please enter your email: ")
-  password = input("please enter a password: ")
-
   #validating password
   if len(password) < 8:
     print("password must be at least 8 characters long")
@@ -78,12 +71,12 @@ def sign_up():
 
     click.echo("User created successfully")
   except Exception as e:
-    click.echo("Error: ", str(e)) 
+    click.echo(f"Error: {str(e)}") 
 
  
 @click.command()
 @click.option('--username', prompt='email', help='enter your email')
-@click.option('--password', prompt=True, hide_input = True, confirmation_promp = True, help='enter your password')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='enter your password')
 def log_in():
     global current_user
     auth = firebase.auth()
@@ -95,11 +88,11 @@ def log_in():
         current_user = user
         click.echo("Login successful! Welcome,", email)
     except Exception as e:
-        click.echo("Error: Incorrect password or email.", str(e))
+        click.echo(f"Error: Incorrect password or email: {str(e)}")
 
 
 @click.command()
-@click.option('--forgot password', prompt='email', help='enter your email')
+@click.option('--email', prompt='enter your email', help='email to reset your passwrod')
 def forgot_password():
   auth = firebase.auth()
   email = input("Please enter your email: ")
@@ -107,7 +100,4 @@ def forgot_password():
     auth.send_password_reset_email(f"email:{email}")
     click.echo("Password reset email sent to your email")
   except Exception as e:
-      click.echo("Error: ", str(e))
-
-    
-
+      click.echo(f"Error: {str(e)}")
